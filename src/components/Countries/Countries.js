31486d2297aa599a81d24d1ce05306abad063d08
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-29 01:41:52 +0530

    added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -16,8 +16,9 @@ const Countries = () => {
            <div className='countries-container'> {/* to show data on UI  */}
                 {
                 // to use Map so each and every data never been missed out.
-                 countries.map(country => <Country country = {country}></Country>)
-                
+                 countries.map(country => <Country 
+                    country = {country}
+                    key = {country.ccn3} ></Country>)
                 }
             </div>
         </div>
--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -4,11 +4,12 @@ const Country = (props) => {
     // console.log(props.country.name.common)
 
     // desctruction are used in here to reduce codes 
-    const {name ,population,area, region,flags} = props.country;
-    // console.log(props.country.flags.png)
+    const {name ,population,area, region,flags,ccn3} = props.country;
+    // console.log(props.country.ccn3)
     return (
         <div className='country'>
             <h3><small> country name</small> : {name.common}</h3>
+            <h4>country code  : {ccn3}</h4>
             <img src={flags.png} alt="" />  {/** this way are used to img by dynamic */}
             <h4>population : {population}</h4>
             <p><b> area no</b> : {area}</p>
